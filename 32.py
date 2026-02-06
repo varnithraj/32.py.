b040a73246dd125d1ee3cce3e7a56b4d2e4ede4d
varnithraj <aero.mksamy@gmail.com>
2026-02-06 07:14:48 +0530

    Add files via upload

---
+++ 32.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(790,800)
+
+pollygon=turtle.Turtle()
+pollygon.forward(100)
+pollygon.right(90)
+pollygon.forward(100)
+pollygon.right(90)
+pollygon.forward(100)
+pollygon.right(90)
+pollygon.forward(100)
+pollygon.right(90)
+turtle.done()
